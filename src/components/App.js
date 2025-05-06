09f09bfc1ccf7945dfececdd9a0dbeeae8481bfe
pajja <pajarskas.matas@gmail.com>
2025-05-06 19:57:29 +0200

    fixing error

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import SnakeGame from "./SnakeGame";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <h1>Matas Pajarskas</h1>
-        <SnakeGame />
       </div>
     );
   }
